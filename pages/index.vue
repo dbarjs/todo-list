17cd086cdf98c6b20b8dbd02732a502395b07c1f
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-15 20:57:05 -0300

    Add home page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout column justify-center align-center>
+    <frame-list></frame-list>
+  </v-layout>
+</template>
+
+<script>
+import FrameList from '../components/FrameList.vue'
+export default {
+  components: {
+    FrameList,
+  },
+  created() {
+    this.$store.dispatch('frames/fetchFrames')
+  },
+}
+</script>
