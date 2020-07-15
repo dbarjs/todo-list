010d24573859b422953206dd2f99978ca24a1f4c
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-15 20:57:51 -0300

    Create FrameList component

---
+++ components/FrameList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <frame v-for="frame in frames" :key="frame.id" :frame-id="frame.id"></frame>
+    <empty-frame></empty-frame>
+  </div>
+</template>
+
+<script>
+import Frame from '../components/Frame.vue'
+import EmptyFrame from '../components/EmptyFrame.vue'
+export default {
+  components: {
+    EmptyFrame,
+    Frame,
+  },
+  computed: {
+    frames() {
+      return this.$store.getters['frames/getFrames']
+    },
+  },
+}
+</script>
