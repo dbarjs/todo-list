7584ee419792222b1b92881cbe57b753be7e1ca5
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-15 20:57:43 -0300

    Create Frame component

---
+++ components/Frame.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card v-if="frame" class="mx-auto" width="320">
+    <v-card-title>{{ frame.title }}</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    frameId: {
+      default: '',
+      type: String,
+    },
+  },
+  computed: {
+    frame() {
+      if (this.frameId) {
+        return this.$store.getters['frames/getFrameById'](this.frameId)
+      } else {
+        return null
+      }
+    },
+  },
+}
+</script>
