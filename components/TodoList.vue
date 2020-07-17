a706f5bce35b2d5b2331b3e8b948cb1bc5b4402a
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-17 11:42:09 -0300

    Create TodoList component

---
+++ components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ todos }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      required: true,
+      default: () => [],
+      type: Array,
+    },
+  },
+}
+</script>
