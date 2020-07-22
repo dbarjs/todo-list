54e45a33a6dd81a8c9896f689c95c91b10510e5b
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-22 05:42:00 -0300

    Add theme cache on localStorage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,21 @@
 <template>
   <v-app dark>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar :clipped-left="clipped" fixed app elevation="2">
       <v-toolbar-title v-text="title" />
+      <v-spacer></v-spacer>
+      <v-btn icon @click="toggleDarkTheme">
+        <v-icon v-if="darkTheme">mdi-brightness-4</v-icon>
+        <v-icon v-else>mdi-brightness-7</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-main>
       <nuxt />
     </v-main>
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+import store from 'store2'
 export default {
   data() {
     return {
@@ -33,7 +36,23 @@ export default {
       right: true,
       rightDrawer: false,
       title: 'To-do List',
+      darkTheme: false,
     }
+  },
+  created() {
+    this.darkTheme = store('@TodoList:dark') || false
+    this.setDarkTheme(this.darkTheme)
+    console.log(this.$vuetify.theme.themes)
+  },
+  methods: {
+    setDarkTheme(state) {
+      this.darkTheme = !!state
+      this.$vuetify.theme.dark = !!state
+      store('@TodoList:dark', !!state)
+    },
+    toggleDarkTheme() {
+      this.setDarkTheme(!this.darkTheme)
+    },
   },
 }
 </script>
