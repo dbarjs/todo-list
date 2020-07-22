a66109745da890b895bd347c6757e5e82c25791d
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-21 23:43:51 -0300

    Add Drag and Drop support

--- components/FrameList.vue
+++ components/FrameList.vue
@@ -1,21 +1,61 @@
 <template>
   <v-container class="frame-list mx-0 full-width">
-    <frame v-for="frame in frames" :key="frame.id" :frame="frame"></frame>
+    <draggable :list="frameList" handle=".handle" @change="onChange">
+      <frame
+        v-for="frame in frameList"
+        :key="frame.id"
+        :frame-id="frame.id"
+      ></frame>
+    </draggable>
     <empty-frame></empty-frame>
   </v-container>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import Frame from '../components/Frame.vue'
 import EmptyFrame from '../components/EmptyFrame.vue'
 export default {
   components: {
+    draggable,
     EmptyFrame,
     Frame,
+  },
+  data() {
+    return {
+      frameList: [],
+    }
   },
   computed: {
     frames() {
       return this.$store.getters['frames/getFrames']
+    },
+  },
+  watch: {
+    frames: {
+      immediate: true,
+      handler(frames) {
+        const frameList = [...frames]
+        frameList.sort((a, b) => (a.order > b.order ? 1 : -1))
+        this.frameList = frameList
+      },
+    },
+  },
+  methods: {
+    onChange({ moved }) {
+      const maxFrameIndex = Math.max(moved.oldIndex, moved.newIndex)
+      const framesToUpdate = this.frameList.filter(
+        (todo, index) => maxFrameIndex >= index,
+      )
+      this.updateOrderOfFrames(framesToUpdate)
+    },
+    updateOrderOfFrames(frames) {
+      frames.forEach((frame, index) => {
+        this.$store.dispatch('frames/updateFrame', {
+          ...frame,
+          order: index,
+        })
+      })
     },
   },
 }
